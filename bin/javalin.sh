69a3350e5d587c01a6c3ecb32817aaaa622c02d8
Mirko Felice <mirko.felice@studio.unibo.it>
2021-04-07 12:57:23 +0200

    Add javalin shell script.

---
+++ bin/javalin.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+executable=./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar
+
+if [ -e ${executable} ]; then
+	mvn clean package
+fi
+
+#REM set JAVA_OPTS=%JAVA_OPTS% -Xmx1024M
+ags="generate -i petstore.json -l javalin -o samples/javalin -DdateLibrary=java.util.Date -DhideGenerationTimestamp=true"
+
+rm -r samples/
+
+java -jar ${executable} "${ags}"
+
+cd samples/javalin
+
+gradlew assemble
